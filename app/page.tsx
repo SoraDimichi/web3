888cecc642299fd4de7c8262469085e886b96c6a
Dmitry Sorokin <soradimichi@gmail.com>
2024-06-24 10:48:38 +0600

    improved algorythm

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,108 @@
 import React, { useEffect, useMemo, HTMLAttributes } from "react";
 import { Web3Provider } from "@ethersproject/providers";
 import { minidenticon } from "minidenticons";
-import { createHash } from "crypto";
 
 type MinidenticonP = { username: string } & HTMLAttributes<HTMLImageElement>;
-const MinidenticonImg = ({ username, ...props }: MinidenticonP) => {
-  const svgURI = useMemo(
-    () =>
-      "data:image/svg+xml;utf8," + encodeURIComponent(minidenticon(username)),
-    [username],
+
+class HLSColor {
+  private lightness: number;
+  private lightnessIndex: number;
+  public color: string;
+
+  constructor(color: string) {
+    this.color = color;
+    this.lightness = this.getLightness();
+    this.lightnessIndex = this.getLightnessIndex();
+  }
+
+  private getLightnessIndex(): number {
+    return this.color.lastIndexOf("%");
+  }
+
+  private getLightness(): number {
+    const substring = this.color.substring(0, this.lightnessIndex);
+    const regex = /[0-9]+(?=[^0-9]*$)/;
+    const [match] = substring.match(regex)!;
+    return Number(match);
+  }
+
+  private replaceLightness(lightness: number) {
+    const beforePercent = this.color.substring(0, this.lightnessIndex);
+
+    const { index: startOfLightness } = beforePercent.match(/(\d+)(?!.*\d)/)!;
+
+    return (
+      this.color.substring(0, startOfLightness) +
+      lightness +
+      this.color.substring(this.lightnessIndex, this.color.length)
+    );
+  }
+
+  public darker(persent: number) {
+    return this.replaceLightness(
+      this.lightness - this.lightness * (persent / 100),
+    );
+  }
+}
+
+const MinidenticonImg = ({ username }: MinidenticonP) => {
+  const svg = useMemo(() => minidenticon(username), [username]);
+  const parser = new DOMParser();
+  const color = parser
+    .parseFromString(svg, "image/svg+xml")
+    .querySelector("svg")!
+    .getAttribute("fill")!;
+  const nickname = generateNickname(username);
+  const background = new HLSColor(color);
+
+  return (
+    <div
+      style={{ background: background.darker(60) }}
+      className="gap-2 rounded-lg max-h-min p-2 text-white flex flex-col items-center justify-center"
+    >
+      <img
+        style={{ background: background.darker(90) }}
+        className="rounded-lg shadow-lg"
+        src={"data:image/svg+xml;utf8," + svg}
+        alt={nickname}
+      />
+      <p
+        style={{ color }}
+        className="capitalize text-lg leading-none font-semibold"
+      >
+        {nickname}
+      </p>
+    </div>
   );
-  return <img src={svgURI} alt={username} {...props} />;
 };
 
+function hexToBase26(hex: string) {
+  const decimal = BigInt("0x" + hex);
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  let base26 = "";
+
+  let number = decimal;
+  while (number > 0) {
+    base26 = alphabet[Number(number % BigInt(26))] + base26;
+    number = number / BigInt(26);
+  }
+
+  return base26;
+}
+
 function generateNickname(ethAddress: string) {
-  const hash = createHash("sha256").update(ethAddress).digest("hex");
-  const letters = hash.replace(/[0-9]/g, "");
+  const without0x = ethAddress.slice(2);
+  const base26 = hexToBase26(without0x);
+  const letters = base26.replace(/[0-9]/g, "").split("").reverse().join("");
+  const vowels = "aeiou";
+
+  console.log(letters);
   let nickname = "";
   let isConsonantNext = true;
+
   for (let char of letters) {
-    const isVowel = "aeiou".includes(char.toLowerCase());
+    const isVowel = vowels.includes(char.toLowerCase());
 
     if (isConsonantNext && !isVowel) {
       nickname += char;
@@ -95,17 +178,7 @@ export default function Home() {
       {/* </div> */}
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        {address && (
-          <div className="gap-2 rounded-lg bg-gray-800 min-h-min p-2 text-white flex flex-col items-center justify-center min-h-screen min-content">
-            <MinidenticonImg
-              className="bg-gray-700 rounded-lg shadow-lg"
-              username={address}
-            />
-            <p className="capitalize text-lg leading-none font-semibold">
-              {generateNickname(address)}
-            </p>
-          </div>
-        )}
+        {address && <MinidenticonImg username={address} />}
 
         {/* <Image */}
         {/*   className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" */}
